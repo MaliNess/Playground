885ab0a3c7763128bb2e42ebeb30c13533dca16f
MaliNess <a.malikova.official@gmail.com>
2018-08-09 16:33:23 +0300

    ...

---
+++ src/minecraft/MinecraftEconomics.java
@@ -0,0 +1,13 @@
+package minecraft;
+
+import minecraft.Item;
+
+public class MinecraftEconomics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Item i = new Item();
+		i.setPrice(10);
+	}
+
+}
