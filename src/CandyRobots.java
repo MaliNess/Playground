e628a6947f3051d74fd904e0d304b6dd33b166d9
MaliNess <a.malikova.official@gmail.com>
2018-08-09 14:36:13 +0300

    cleaning

--- src/CandyRobots.java
+++ src/CandyRobots.java
@@ -30,6 +30,7 @@ public class CandyRobots {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner in = new Scanner(System.in);
 		Random r = new Random();
 
